2b2fc6a3dec17f05897ed52742a33d708a932b9c
Emil Sharkov <emosharkov@gmail.com>
2022-12-23 10:25:49 -0800

    added .env file

--- server/database/database.js
+++ server/database/database.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
+const ckey = require('ckey')
 
 const pool = new Pool({
-    user: 'docker',
-    password: 'docker',
-    host: "database",
-    port: 4000,
-    database: "docker"
+    user: ckey.DB_USER || 'docker',
+    password: ckey.DB_PASSWORD || 'docker',
+    host: ckey.DB_HOST || "database",
+    port: ckey.DB_PORT || 4000,
+    database: ckey.DB_DATABASE || "docker"
 })
-
 module.exports = pool;
